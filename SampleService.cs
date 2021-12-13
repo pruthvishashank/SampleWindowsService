e2c6bb4c49dcf8f7fc851b4e533a2a9c27a9071c
mshaik07 <mahaboobsubhani.shaik@factset.com>
2021-12-13 11:56:52 +0530

    Add project files.

---
+++ SampleService.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+
+namespace SampleWindowsService
+{
+    public partial class SampleService : ServiceBase
+    {
+        public SampleService()
+        {
+            InitializeComponent();
+        }
+        private Timer Schedular;
+        protected override void OnStart(string[] args)
+        {
+            this.WriteToFile("Sample Service started !!");
+            this.ScheduleService();
+        }
+
+        protected override void OnStop()
+        {
+            this.WriteToFile("Sample Service Stopped !!");
+            this.Schedular.Dispose();
+        }
+        public void ScheduleService()
+        {
+            try
+            {
+                Schedular = new Timer(new TimerCallback(SchedularCallback));
+                string mode = "INTERVAL";
+                this.WriteToFile("Simple Service Mode: " + mode + " {0}");
+
+                //Set the Default Time.
+                DateTime scheduledTime = DateTime.MinValue;
+
+                if (mode == "DAILY")
+                {
+                    //Get the Scheduled Time from AppSettings.
+                    scheduledTime = DateTime.Parse("18:41");
+                    if (DateTime.Now > scheduledTime)
+                    {
+                        //If Scheduled Time is passed set Schedule for the next day.
+                        scheduledTime = scheduledTime.AddDays(1);
+                    }
+                }
+
+                if (mode.ToUpper() == "INTERVAL")
+                {
+                    //Get the Interval in Minutes from AppSettings.
+                    int intervalMinutes = Convert.ToInt32(5);
+
+                    //Set the Scheduled Time by adding the Interval to Current Time.
+                    scheduledTime = DateTime.Now.AddMinutes(intervalMinutes);
+                    if (DateTime.Now > scheduledTime)
+                    {
+                        //If Scheduled Time is passed set Schedule for the next Interval.
+                        scheduledTime = scheduledTime.AddMinutes(intervalMinutes);
+                    }
+                }
+
+                TimeSpan timeSpan = scheduledTime.Subtract(DateTime.Now);
+                string schedule = string.Format("{0} day(s) {1} hour(s) {2} minute(s) {3} seconds(s)", timeSpan.Days, timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+
+                this.WriteToFile("Simple Service scheduled to run after: " + schedule + " {0}");
+
+                //Get the difference in Minutes between the Scheduled and Current Time.
+                int dueTime = Convert.ToInt32(timeSpan.TotalMilliseconds);
+
+                //Change the Timer's Due Time.
+                Schedular.Change(dueTime, Timeout.Infinite);
+            }
+            catch (Exception ex)
+            {
+                WriteToFile("Simple Service Error on: {0} " + ex.Message + ex.StackTrace);
+
+                //Stop the Windows Service.
+                using (System.ServiceProcess.ServiceController serviceController = new System.ServiceProcess.ServiceController("SimpleService"))
+                {
+                    serviceController.Stop();
+                }
+            }
+        }
+
+        private void SchedularCallback(object e)
+        {
+            this.WriteToFile("Simple Service Log: {0}");
+            this.ScheduleService();
+        }
+
+        private void WriteToFile(string text)
+        {
+            string dirPath = "C:\\ServiceLogs\\";
+            if (!Directory.Exists(dirPath))
+            {
+                Directory.CreateDirectory(dirPath);
+            }
+            string path =Path.Combine(dirPath, $"{DateTime.Now.ToString("MM-dd-yyyy")}_SampleServiceLog.txt");
+            using (StreamWriter writer = new StreamWriter(path, true))
+            {
+                writer.WriteLine(string.Format(text, DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss tt")));
+                writer.Close();
+            }
+        }
+    }
+}
